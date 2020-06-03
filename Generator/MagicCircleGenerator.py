915c4f14edaecce1e4d17c8df4947f86684d0b9e
kinglegon <jessenbeaupre@gmail.com>
2020-06-03 17:28:20 -0500

    Added liscense for the font, refactored code so it was using the python standard for naming conventions, seperated reused properties into it's own class and added a proper main function.

--- Generator/MagicCircleGenerator.py
+++ Generator/MagicCircleGenerator.py
@@ -5,97 +5,111 @@ from random import randint
 import random
 
 
+class ImageProperties:
+    def __intit__(self, args):
+        # sets a size variable from the size argument
+        self.image_size = args.size
+        # creates square background from inputs
+        self.image = Image.new("RGBA", (self.image_size, self.image_size), (args.background[0], args.background[1], \
+                                                                            args.background[2], args.background[3]))
+        # sets up drawing
+        self.painter = ImageDraw.Draw(self.image, "RGBA")
+
+
 # draw small circles
-def drawCircle(x, y, size):
-    d.ellipse(((x-size, y-size), (x+size, y+size)), fill=None, outline = (0 ,0 ,0), width=int(size*0.1))
+def draw_circle(x, y, size, imgProp):
+    imgProp.painter.ellipse(((x-size, y-size), (x+size, y+size)), fill=None, outline=(0, 0, 0), width=int(size*0.1))
 
 
 # places an individual letter
-def drawLetter(x, y, letter, font):
-    d.text((x, y), letter, font=font, fill=(0,0,0,255))
+def draw_letter(x, y, letter, font, imgProp):
+    imgProp.painter.text((x, y), letter, font=font, fill=(0,0,0,255))
 
 
-def circleCenter():
+def circle_center(img_prop):
     # loop to draw circles in a circle
-    circleRadius = int(imageSize * 0.2)
+    circleRadius = int(img_prop.image_size * 0.2)
     circleCount = randint(2, 12)
     for i in range(circleCount):
-        circleX = int(imageSize / 2) + circleRadius * math.cos(2 * math.pi * i / circleCount)
-        circleY = int(imageSize / 2) + circleRadius * math.sin(2 * math.pi * i / circleCount)
-        drawCircle(circleX, circleY, int(circleRadius * 0.50))
+        circleX = int(img_prop.image_size / 2) + circleRadius * math.cos(2 * math.pi * i / circleCount)
+        circleY = int(img_prop.image_size / 2) + circleRadius * math.sin(2 * math.pi * i / circleCount)
+        draw_circle(circleX, circleY, int(circleRadius * 0.50), img_prop)
     # center circle for style
-    drawCircle(imageSize / 2, imageSize / 2, imageSize / 10)
+    draw_circle(img_prop.image_size / 2, img_prop.image_size / 2, img_prop.image_size / 10, img_prop)
 
 
-def textCircleCenter():
+def text_circle_center(img_prop):
     # loop to draw circles in a circle
-    circleCount = randint(2, 5)
-    circleRadius = int(imageSize * 0.15) + circleCount * 6
-    for i in range(circleCount):
-        circleX = int(imageSize / 2) + circleRadius * math.cos(2 * math.pi * i / circleCount)
-        circleY = int(imageSize / 2) + circleRadius * math.sin(2 * math.pi * i / circleCount)
+    circle_count = randint(2, 5)
+    circle_radius = int(img_prop.image_size * 0.15) + circle_count * 6
+    for i in range(circle_count):
+        circle_x = int(img_prop.image_size / 2) + circle_radius * math.cos(2 * math.pi * i / circle_count)
+        circle_y = int(img_prop.image_size / 2) + circle_radius * math.sin(2 * math.pi * i / circle_count)
         # draw outer then inner circle
-        drawCircle(circleX, circleY, int(circleRadius * 1) - circleCount * 14)
-        drawCircle(circleX, circleY, int(((circleRadius * 1) - circleCount * 14) * 0.6))
+        draw_circle(circle_x, circle_y, int(circle_radius * 1) - circle_count * 14, img_prop)
+        draw_circle(circle_x, circle_y, int(((circle_radius * 1) - circle_count * 14) * 0.6), img_prop)
 
         # add text to inner circles
-        innerCircleRadius = int(((circleRadius * 1) - circleCount * 14) * 0.8)
-        innerText = generateText(6, 10)
-        fnt = ImageFont.truetype('dejavuserif.ttf', int((imageSize * .04) - (len(innerText) / 2)))
-        innerTextCircleCount = len(innerText)
-        for i in range(len(innerText)):
-            innerTextCircleX = int(circleX) + innerCircleRadius * math.cos(2 * math.pi * i / innerTextCircleCount)
-            innerTextCircleY = int(circleY) + innerCircleRadius * math.sin(2 * math.pi * i / innerTextCircleCount)
-            drawLetter(innerTextCircleX - innerCircleRadius * 0.1, innerTextCircleY - innerCircleRadius * 0.2, innerText[i], fnt)
+        inner_circle_radius = int(((circle_radius * 1) - circle_count * 14) * 0.8)
+        inner_text = generate_text(6, 10)
+        fnt = ImageFont.truetype('dejavuserif.ttf', int((img_prop.image_size * .04) - (len(inner_text) / 2)))
+        inner_text_circle_count = len(inner_text)
+        for i in range(len(inner_text)):
+            inner_text_circle_x = int(circle_x) + inner_circle_radius * math.cos(2 * math.pi * i / inner_text_circle_count)
+            inner_text_circle_y = int(circle_y) + inner_circle_radius * math.sin(2 * math.pi * i / inner_text_circle_count)
+            draw_letter(inner_text_circle_x - inner_circle_radius * 0.1, inner_text_circle_y - inner_circle_radius * 0.2, inner_text[i], fnt, img_prop)
 
 
-def starCenter():
+def star_center(img_prop):
     # sets number of points to generate
     points = randint(3, 13)
     # sets random offset for the points to start
     offset = random.random()
     # creates starting of first line
-    oldx = int(imageSize/2) + int(imageSize * 0.3) * math.sin(2 * math.pi * offset)
-    oldy = int(imageSize/2) + int(imageSize * 0.3) * math.cos(2 * math.pi * offset)
+    old_x = int(img_prop.image_size / 2) + int(img_prop.image_size * 0.3) * math.sin(2 * math.pi * offset)
+    old_y = int(img_prop.image_size / 2) + int(img_prop.image_size * 0.3) * math.cos(2 * math.pi * offset)
     # sets the sequence number at max to start to equal 1 on later calculations with division
-    currentSequence = points
-    # creates iterator so it makes staisfying star shapes relative to number of points
+    current_sequence = points
+    # creates iterator so it makes satisfying star shapes relative to number of points
     iterator = math.ceil(points / 2) - 1
 
     # loops through each point
     for i in range(points):
         # sets which point in the sequence it will do and sets it back down if it goes above the max
-        currentSequence = currentSequence + iterator
-        if currentSequence > points:
-            currentSequence -= points
+        current_sequence = current_sequence + iterator
+        if current_sequence > points:
+            current_sequence -= points
         # sets the rotation relative to the center for graphing in range 0-1, sets it back down if it goes out of range
-        rotation = currentSequence / points + offset
+        rotation = current_sequence / points + offset
         if rotation > 1:
             rotation -= 1
         # sets the destination of the line
-        newx = int(imageSize/2) + int(imageSize * 0.3) * math.sin(2 * math.pi * rotation)
-        newy = int(imageSize/2) + int(imageSize * 0.3) * math.cos(2 * math.pi * rotation)
+        new_x = int(img_prop.image_size / 2) + int(img_prop.image_size * 0.3) * math.sin(2 * math.pi * rotation)
+        new_y = int(img_prop.image_size / 2) + int(img_prop.image_size * 0.3) * math.cos(2 * math.pi * rotation)
         # writes the line from starting to end point
-        d.line((oldx, oldy, (newx, newy)), fill=(0, 0, 0), width=int(imageSize / 80))
+        img_prop.painter.line((old_x, old_y, (new_x, new_y)), fill=(0, 0, 0), width=int(img_prop.image_size / 80))
         # sets the next starting point to the current end of the line
-        oldx = newx
-        oldy = newy
+        old_x = new_x
+        old_y = new_y
 
-def concentericCircles():
+
+def concenteric_circles(img_prop):
     # sets a starting radius for the circles
-    concentricCircleRadius = imageSize * 0.3
+    concentric_circle_radius = img_prop.image_size * 0.3
     # sets up the center of the image for later use
-    imageMiddle = imageSize / 2
+    image_middle = img_prop.image_size / 2
     # loops until the radius of the circles gets too small to bother
-    while concentricCircleRadius > imageSize * 0.01:
+    while concentric_circle_radius > img_prop.image_size * 0.01:
         # draws a circle starting from the middle outward the radius amount
-        d.ellipse(((imageMiddle - concentricCircleRadius, imageMiddle - concentricCircleRadius), (imageMiddle + concentricCircleRadius, imageMiddle + concentricCircleRadius))\
-                  , fill=None, outline = (0 ,0 ,0), width=int(concentricCircleRadius * 0.05))
+        img_prop.painter.ellipse(((image_middle - concentric_circle_radius, image_middle - concentric_circle_radius), \
+                                  (image_middle + concentric_circle_radius, image_middle + concentric_circle_radius)) \
+                                 , fill=None, outline = (0 ,0 ,0), width=int(concentric_circle_radius * 0.05))
         # makes the radius smaller for the next circle
-        concentricCircleRadius *= 0.9
+        concentric_circle_radius *= 0.9
 
-def generateText(minLetter, maxLetter):
-    # sets chracater variable and a unicode range to pull from
+
+def generate_text(min_letter, max_letter):
+    # sets character variable and a unicode range to pull from
     get_char = chr
     unicode_range = (0x0021, 0x038C)
     # creates a list of chars to pull from based on all the chars in the unicode range
@@ -103,64 +117,66 @@ def generateText(minLetter, maxLetter):
         get_char(char_point) for char_point in range(unicode_range[0], unicode_range[1])
     ]
     # returns a string with random choices from the alphabet list
-    return''.join([random.choice(alphabet) for n in range(randint(minLetter, maxLetter))])
+    return''.join([random.choice(alphabet) for n in range(randint(min_letter, max_letter))])
 
 
-def getArgs():
+def get_args():
     # gets command line arguments to set background and circle size
-    parser = argparse.ArgumentParser(description="enter image size(in pixels) and RGBA color code tuple for the background")
+    parser = argparse.ArgumentParser(description="Enter image size(in pixels) and RGBA color code tuple for the background")
     parser.add_argument('size', type=int, nargs="?", default=1000, help='Size of the image')
-    parser.add_argument('background', type=int, nargs=argparse.REMAINDER, default=(255, 255, 255, 255), help='background color of the circle in RGBA')
+    parser.add_argument('background', type=int, nargs=argparse.REMAINDER, default=(255, 255, 255, 255), help='Background color of the circle in RGBA')
     return parser.parse_args()
 
 
-# gets command line args
-args = getArgs()
-# sets transparency to full if it wasn't specified
-if len(args.background) == 3:
-    args.background.append(255)
-# deals with if there are no args for background and sets it to default white
-elif len(args.background) < 4:
-    args.background = (255, 255, 255, 255)
+def main():
+    # gets command line args
+    args = get_args()
+    # sets transparency to full if it wasn't specified
+    if len(args.background) == 3:
+        args.background.append(255)
+    # deals with if there are no args for background and sets it to default white
+    elif len(args.background) < 4:
+        args.background = (255, 255, 255, 255)
 
-# sets a size variable from the size argument
-imageSize = args.size
+    # Creates image properties object
+    img_prop = ImageProperties()
+    img_prop.__intit__(args)
 
-# creates square background from inputs
-im = Image.new("RGBA", (imageSize, imageSize), (args.background[0], args.background[1], args.background[2], args.background[3]))
+    # draws outer circles based on image size
+    img_prop.painter.ellipse(((img_prop.image_size * 0.10, img_prop.image_size * 0.10), (img_prop.image_size * 0.90, img_prop.image_size * 0.90)), \
+                             fill=None, outline=(0, 0, 0), width=int(img_prop.image_size / 80))
+    img_prop.painter.ellipse(((img_prop.image_size * 0.2, img_prop.image_size * 0.2), (img_prop.image_size * 0.8, img_prop.image_size * 0.8)), \
+                             fill=None, outline=(0, 0, 0), width=int(img_prop.image_size / 80))
 
-# sets up drawing
-d = ImageDraw.Draw(im, "RGBA")
+    # sets random circle text
+    text = generate_text(10, 40)
+    # initialize font
+    fnt = ImageFont.truetype('dejavuserif.ttf', int((img_prop.image_size * .08) - (len(text) / 2)))
+    # places text in the middle of the 2 base circles
+    circle_radius = int(img_prop.image_size * 0.35)
+    text_circle_count = len(text)
+    for i in range(len(text)):
+        text_circle_x = int(img_prop.image_size / 2) + circle_radius * math.cos(2 * math.pi * i / text_circle_count)
+        text_circle_y = int(img_prop.image_size / 2) + circle_radius * math.sin(2 * math.pi * i / text_circle_count)
+        # some adjustments to x and y so they fit into the circle better
+        draw_letter(text_circle_x - (img_prop.image_size * .02), text_circle_y - (img_prop.image_size * .04), text[i], fnt, img_prop)
 
-# draws outer circles based on image size
-d.ellipse(((imageSize * 0.10, imageSize * 0.10), (imageSize * 0.90, imageSize * 0.90)), fill=None, outline=(0, 0, 0), width=int(imageSize / 80))
-d.ellipse(((imageSize * 0.2, imageSize * 0.2), (imageSize * 0.8, imageSize * 0.8)), fill=None, outline=(0, 0, 0), width=int(imageSize / 80))
+    # choses what the inner circle type will be out of a list and runs it
+    switcher = {
+        1: circle_center,
+        2: text_circle_center,
+        3: star_center,
+        4: concenteric_circles,
+        5: star_center,
+        6: text_circle_center,
+    }
+    # gets and runs the function for the corresponding inner circle
+    inner_style_function = switcher.get(randint(1, 6))
+    inner_style_function(img_prop)
 
-# sets random circle text
-text = generateText(10, 40)
-# intialize font
-fnt = ImageFont.truetype('dejavuserif.ttf', int((imageSize * .08) - (len(text) / 2)))
-# places text in the middle of the 2 base circles
-circleRadius = int(imageSize * 0.35)
-textCircleCount = len(text)
-for i in range(len(text)):
-    textCircleX = int(imageSize / 2) + circleRadius * math.cos(2 * math.pi * i / textCircleCount)
-    textCircleY = int(imageSize / 2) + circleRadius * math.sin(2 * math.pi * i / textCircleCount)
-    # some adjustments to x and y so they fit into the circle better
-    drawLetter(textCircleX - (imageSize * .02), textCircleY - (imageSize * .04), text[i], fnt)
+    # saves the image
+    img_prop.image.save("MagicCircle.png")
 
-# choses what the inner circle type will be out of a list and runs it
-switcher = {
-    1:circleCenter,
-    2:textCircleCenter,
-    3:starCenter,
-    4:concentericCircles,
-    5:starCenter,
-    6:textCircleCenter,
-}
-# gets and runs the function for the corresponding inner circle
-innerStyleFunction = switcher.get(randint(1, 6))
-innerStyleFunction()
 
-# saves the image
-im.save("magicCircle.png")
+if __name__ == "__main__":
+    main()
